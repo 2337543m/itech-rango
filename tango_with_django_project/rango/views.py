8ceceff4be87539fcde3335cd9ae9c597b71868c
Roddy <roddy.mcneill1@live.com>
2018-01-16 16:08:58 +0000

    added templates

--- tango_with_django_project/rango/views.py
+++ tango_with_django_project/rango/views.py
@@ -2,6 +2,9 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-	return HttpResponse("all around me are familiar faces")
+	context_dict = {'boldmessage' : "I'm blue daba dee daba die"}
+	return render(request, 'rango/index.html', context=context_dict)
 
+def about(request):
+	return HttpResponse("there's a moose loose aboot this hoose <a href='/rango/'>Index</a>.")
 # Create your views here.
