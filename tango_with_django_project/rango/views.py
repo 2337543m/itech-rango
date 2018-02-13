ed6e868c3710120a2bd640c73cb335f56ea30c58
Roddy <roddy.mcneill1@live.com>
2018-02-13 01:25:17 +0000

    10-10.5

--- tango_with_django_project/rango/views.py
+++ tango_with_django_project/rango/views.py
@@ -5,15 +5,22 @@ from rango.forms import CategoryForm, PageForm, UserForm, UserProfileForm
 from django.contrib.auth import authenticate, login, logout
 from django.core.urlresolvers import reverse
 from django.contrib.auth.decorators import login_required
+from datetime import datetime
 
 def index(request):
 	category_list = Category.objects.order_by('-likes')[:5]
 	page_list = Page.objects.order_by('-views')[:5]
 	context_dict = {'categories': category_list, 'pages': page_list}
-	return render(request, 'rango/index.html', context_dict)
+	
+	response = render(request, 'rango/index.html', context_dict)
+	visitor_cookie_handler(request, response)
+	return response
 
 def about(request):
-	context_dict = {'aboutmessage' : "This tutorial has een put together by Lord Awesome"}
+	if request.session.test_cookie_worked():
+		print("TEST COOKIE WORKED!")
+		request.session.delete_test_cookie()
+	context_dict = {'aboutmessage' : "This tutorial has been put together by Lord Awesome"}
 	return render(request, 'rango/about.html', context=context_dict)
 	
 def show_category(request, category_name_slug):
@@ -117,5 +124,20 @@ def restricted(request):
 def user_logout(request):
 	logout(request)
 	return HttpResponseRedirect(reverse('index'))
-	# Create your views here.
+	
+def visitor_cookie_handler(request, response):
+	visits = int(request.COOKIES.get('visits', '1'))
+	last_visit_cookie = request.COOKIES.get('last_visit', str(datetime.now()))
+	last_visit_time = datetime.strptime(last_visit_cookie[:-7], '%Y-%m-%d %H:%M:%S')
+	if (datetime.now() - last_visit_time).days > 0:
+		visits = visits + 1
+		response.set_cookie('last_visit', str(datetime.now()))
+	else:
+		visits = 1
+		response.set_cookie('last_visit', last_visit_cookie)
+	
+	response.set_cookie('visits', visits)
+		
+		
+# Create your views here.
 
